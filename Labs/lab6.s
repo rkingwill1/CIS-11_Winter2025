b0652d58859361d02dfd14b4f1edf4e87d655deb
rkingwill1 <rkingwill1@student.rccd.edu>
2025-01-15 18:23:52 -0800

    Add lab6.s

--- Labs/lab6.s
+++ Labs/lab6.s
@@ -1,13 +1,14 @@
 .global main
 
 .section .data //Initialized variables
-	a: .word 0
-	b: .word 0
-	c: .word 0
+	a: .word 0 //Integer 1
+	b: .word 0 //Integer 2
+	c: .word 0 //Integer 3
 
 .section .rodata //Readonly Data
 	inPat: .asciz "%d"
-	prompt: "Enter an integer: "
+	prompt: .asciz "Enter an integer: "
+	outStr: .asciz "Largest Integer: %d\n"
 	
 
 .section .bss //Uninitialized Variables
@@ -43,22 +44,33 @@ main:
 
 	//Comparisons
 	//Load registers with a & b
-	ldr r0, a
-	ldr r1, b
+	ldr r1, =a
+	ldr r1, [r1] //Dereference a
+	ldr r2, =b
+	ldr r2, [r2] //Dereference b
 	//Compare a and b
-	cmp r0, r1
-	bgt CmpXC
+	cmp r1, r2
+	bgt CmpXC //if(a>b), don't move anything
 
-LessAB:
-	ldr r0, =b
+LessAB: //If a < b
+	mov r1, r2 //Put b in r1
 	
-CmpXC:
-	ldr r1, =c
-	bgt
+CmpXC: //Compare either a or b with c
+	ldr r2, =c //Put c in r2
+	ldr r2, [r2] //Dereference c
+	cmp r1, r2 //Compare r1 (a or b) with r2 (c)
+	bgt Output
 
-
-
-
-
+LessXC: //If (either a or b) < c
+    mov r1, r2 //Put c into r1 
+    
+Output:
+	//Output result
+    ldr r0, =outStr
+    bl printf
+    
+	//return 0
+    mov r0, #0
+	
 	pop {pc}
-
+	
